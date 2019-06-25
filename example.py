f9546feeaf743e1ac0f74f782cf164d459db2c86
jonathanvanschenck <44685047+jonathanvanschenck@users.noreply.github.com>
2019-06-25 16:19:54 -0700

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import chopper,time
 
 ch = chopper.CHOPPER(verbose=True)
-ch.printTerminal
+ch.printTerminal()
 ch.setSpinValue(100)
 ch.startSpin()
 time.sleep(10)#Allow time for chopper motor to reach stable spin speed
